4781ea5c9c14003f09c6e4d5f242ae7e4508b5b1
YutongDai1 <yutong.dai.apply@gmail.com>
2024-07-24 13:12:27 +0800

    third commit for elevator system

--- ElevatorSystem/src/main/java/org/design/ElevatorFactory.java
+++ ElevatorSystem/src/main/java/org/design/ElevatorFactory.java
@@ -1,5 +1,12 @@
 package org.design;
 
 public class ElevatorFactory {
+  public Elevator createElevator(ElevatorType type) {
+    return switch (type) {
+      case PASSENGER -> new PassengerElevator(1, false);
+      case SERVICE -> new ServiceElevator(1, false);
+      default -> throw new IllegalArgumentException("Unknown criteria.");
+    };
+  }
 
 }
--- ElevatorSystem/src/main/java/org/design/ServiceRequest.java
+++ ElevatorSystem/src/main/java/org/design/ServiceRequest.java
@@ -1,8 +1,14 @@
 package org.design;
 
 public class ServiceRequest extends Request{
-  public ServiceRequest(RequestOrigin origin, int currentFloor, int destinationFloor) {
+  private double weight;
+  public ServiceRequest(RequestOrigin origin, int currentFloor, int destinationFloor, int weight) {
     super(origin, currentFloor, destinationFloor);
     this.elevatorType = ElevatorType.SERVICE;
+    this.weight = weight;
+  }
+
+  public double getWeight() {
+    return weight;
   }
 }
